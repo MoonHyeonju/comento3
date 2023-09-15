2a96634c6e41280cdf5b2d15523127b7ef41fd77
MoonHyeonju <100399677+MoonHyeonju@users.noreply.github.com>
2023-09-15 21:56:21 +0900

    Add files via upload

---
+++ settingweb_boot/src/main/java/com/comento/settingweb_boot/test/SettingTest.java
@@ -0,0 +1,27 @@
+package com.comento.settingweb_boot.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SettingTest {
+
+    @RequestMapping("/test")
+    public ModelAndView test() throws Exception {
+        ModelAndView mav = new ModelAndView("test");
+        mav.addObject("name", "devfunpj");
+        List<String> resultList = new ArrayList<String>();
+        resultList.add("!!!HELLO WORLD!!!");
+        resultList.add("설정 TEST!!!");
+        resultList.add("설정 TEST!!!");
+        resultList.add("설정 TEST!!!!!");
+        resultList.add("설정 TEST!!!!!!");
+        mav.addObject("list", resultList);
+        return mav;
+
+    }
+}
